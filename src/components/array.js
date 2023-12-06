f3cb4e9882461e75c3cfad6af8347555b9f95b78
Shahrukh-HIC074 <123742184+Shahrukh-HIC074@users.noreply.github.com>
2023-12-06 16:34:50 +0530

    shahrukh

---
+++ src/components/array.js
@@ -0,0 +1,23 @@
+// Filename - components/array.js
+
+// Javascript object named array 
+// with 3 key-values
+const array = [
+	{
+		id: "1",
+		Name: "Shahrukh",
+		Age: "23",
+	},
+	{
+		id: "2",
+		Name: "Sujit",
+		Age: "22",
+	},
+	{
+		id: "3",
+		Name: "Vivek",
+		Age: "23",
+	},
+];
+
+export default array;
